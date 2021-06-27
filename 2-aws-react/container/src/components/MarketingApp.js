05b0ada28feb94fe8d439441f53b495f0be6f029
Francisco PG <hellofrancijpg@gmail.com>
2021-06-27 09:09:41 -0400

    Update MarketingApp.js

--- 2-aws-react/container/src/components/MarketingApp.js
+++ 2-aws-react/container/src/components/MarketingApp.js
@@ -1,6 +1,6 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
@@ -8,13 +8,14 @@ export default () => {
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
         if (pathname !== nextPathname) {
           history.push(nextPathname);
         }
-      }
+      },
     });
 
     history.listen(onParentNavigate);
